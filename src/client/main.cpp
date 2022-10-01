db6fc0dd020bc6798fb66c14d210c86af5aeb344
Nydragon <6582485n@gmail.com>
2022-10-01 14:29:04 +0200

    [Feature] added health, armor, collision components

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,20 +2,44 @@
 // #include "../shared/ECS/ECS.hpp"
 #include "../shared/ECS/Components.hpp"
 
-int main() {
+int main()
+{
     std::cout << "Client: Hello, World!" << std::endl;
 
-    std::cout << getComponentID() << std::endl;
-    std::cout << getComponentID() << std::endl;
-
     Manager manager;
-    auto& newPlayer(manager.addEntity());
-    newPlayer.addComponent<PositionComponent>();
+    auto &newPlayer(manager.addEntity());
 
-    std::cout << newPlayer.getComponent<PositionComponent>().x() << std::endl;
-    std::cout << newPlayer.getComponent<PositionComponent>().y() << std::endl;
+    newPlayer.addComponent<Position>();
+
+    std::cout << "x: " << newPlayer.getComponent<Position>().x() << ", y: " << newPlayer.getComponent<Position>().y() << std::endl;
+
     std::cout << newPlayer.isActive() << std::endl;
     newPlayer.destroy();
     std::cout << newPlayer.isActive() << std::endl;
+
+    auto &zorg(manager.addEntity());
+
+    newPlayer.addComponent<Health>();
+
+    newPlayer.getComponent<Health>().setValues(10);
+
+    zorg.addComponent<Health>();
+
+    zorg.getComponent<Health>().setValues(10);
+
+    zorg.addComponent<CollisionEffect>();
+
+    newPlayer.addComponent<CollisionEffect>();
+
+    newPlayer.getComponent<CollisionEffect>().setEffect(1, 0);
+
+    zorg.getComponent<CollisionEffect>().setEffect(3, 0);
+
+    std::cout << newPlayer.getComponent<Health>().getHealth() << zorg.getComponent<Health>().getHealth() << std::endl;
+
+    zorg.getComponent<CollisionEffect>().collide(newPlayer);
+
+    std::cout << newPlayer.getComponent<Health>().getHealth() << zorg.getComponent<Health>().getHealth() << std::endl;
+
     return 0;
 }
--- src/shared/ECS/ECS.hpp
+++ src/shared/ECS/ECS.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <array>
 #include <bitset>
@@ -14,14 +14,14 @@ using ComponentID = std::size_t;
 using Bitset = std::bitset<maxBitsetSize>;
 using ComponentArray = std::array<Component *, maxBitsetSize>;
 
-
 inline ComponentID getComponentID()
 {
     static ComponentID lastID = 0;
     return lastID++;
 }
 
-template <typename T> inline ComponentID getComponentID() noexcept
+template <typename T>
+inline ComponentID getComponentID() noexcept
 {
     static ComponentID ID = getComponentID();
     return ID;
@@ -29,53 +29,57 @@ template <typename T> inline ComponentID getComponentID() noexcept
 
 class Component
 {
-    public:
-        Entity* entity;
+public:
+    Entity *entity;
 
-        virtual void init() {}
-        virtual void update() {}
-        virtual void draw() {}
+    virtual void init() {}
+    virtual void update() {}
+    virtual void draw() {}
 
-        virtual ~Component() {}
+    virtual ~Component() {}
 };
 
 class Entity
 {
-private: 
+private:
     bool active = true;
-    
-    std::vector<std::unique_ptr<Component>> components;
 
+    std::vector<std::unique_ptr<Component>> components;
 
     Bitset componentBitset;
     ComponentArray componentArray;
 
 public:
-    void draw () {
-        for (auto &c : this->components) {
+    void draw()
+    {
+        for (auto &c : this->components)
+        {
             c->draw();
         }
     }
 
-    void update () {
-        for (auto &c : this->components) {
+    void update()
+    {
+        for (auto &c : this->components)
+        {
             c->update();
         }
     }
 
-    template<typename T>
-    bool hasComponent() {
+    template <typename T>
+    bool hasComponent()
+    {
         return componentBitset[getComponentID<T>()];
     }
 
     template <typename T>
-    T& addComponent()
+    T &addComponent()
     {
         // T c0 = T();
         // T* c = &c0;
-        T* c = new T();
+        T *c = new T();
         c->entity = this;
-        std::unique_ptr<Component> uPtr { c };
+        std::unique_ptr<Component> uPtr{c};
         components.emplace_back(std::move(uPtr));
         componentArray[getComponentID<T>()] = c;
         componentBitset[getComponentID<T>()] = true;
@@ -83,45 +87,57 @@ public:
         return *c;
     }
 
-    template<typename T>
-    T& getComponent() const {
+    template <typename T>
+    T &getComponent() const
+    {
         auto ptr(componentArray[getComponentID<T>()]);
-        return *static_cast<T*>(ptr);
+        return *static_cast<T *>(ptr);
     }
 
-    void destroy() {
+    void destroy()
+    {
         this->active = false;
     }
 
-    bool isActive() const {
+    bool isActive() const
+    {
         return this->active;
-    }       
+    }
 };
 
 class Manager
 {
-    private:
-        std::vector<std::unique_ptr<Entity>> entities;
-    public:
-        void draw() {
-            for (auto& e : entities)
-                e->draw();
-        }
-        void update() {
-            for (auto& e : entities)
-                e->update();
-        }
-        void refresh() {
-            for (size_t i = 0; i < entities.size(); i++)
-                if (!(*entities[i]).isActive()) {
-                    entities.erase(entities.begin() + i);
-                    i--;
-                }
-        }
-        Entity& addEntity() {
-            Entity* e = new Entity();
-            std::unique_ptr<Entity> uPtr {e};
-            entities.emplace_back(std::move(uPtr));
-            return *e;
-        }
+private:
+    std::vector<std::unique_ptr<Entity>> entities;
+
+public:
+    void draw()
+    {
+        for (auto &e : entities)
+            e->draw();
+    }
+
+    void update()
+    {
+        for (auto &e : entities)
+            e->update();
+    }
+
+    void refresh()
+    {
+        for (size_t i = 0; i < entities.size(); i++)
+            if (!(*entities[i]).isActive())
+            {
+                entities.erase(entities.begin() + i);
+                i--;
+            }
+    }
+
+    Entity &addEntity()
+    {
+        Entity *e = new Entity();
+        std::unique_ptr<Entity> uPtr{e};
+        entities.emplace_back(std::move(uPtr));
+        return *e;
+    }
 };
