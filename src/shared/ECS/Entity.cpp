846dadbd0b1c6afed3aee033ebc80e221bcba248
Nydragon <6582485n@gmail.com>
2022-10-03 17:37:06 +0200

    [Feature] add updating off a component via string deserialization

--- src/shared/ECS/Entity.cpp
+++ src/shared/ECS/Entity.cpp
@@ -22,7 +22,18 @@ std::string Entity::serialize() const {
     return str;
 }
 
-void Entity::deserialize(std::string entity_string) {
+std::vector<int> buildArgsVect(std::string argsStr) {
+    std::vector<int> args;
+    std::string token;
+    std::stringstream ss(argsStr);
+
+    while (getline(ss, token, ',')) {
+        args.push_back(stoi(token));
+    }
+    return args;
+}
+
+void Entity::deserialize(const std::string entity_string) {
     components type;
 
     std::regex component_names("([[:alpha:]]+)+", std::regex_constants::egrep);
@@ -31,19 +42,25 @@ void Entity::deserialize(std::string entity_string) {
 
     for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
         const std::string componentName = (*i).str();
-        std::cout << "componentName: " << componentName << std::endl;
+
+        std::regex argsReg(std::string("") + componentName + "\\(([-\\d,]+?)\\)", std::regex_constants::ECMAScript);
+        std::smatch match;
+
+        std::regex_search(entity_string.begin(), entity_string.end(), match, argsReg);
+        std::vector<int> args = buildArgsVect(match[1]);
+
         switch (type[componentName]) {
         case componentsEnum::HEALTH:
-            this->updateComponent<Health>(1);
+            this->updateComponent<Health>(args[0]);
             break;
         case componentsEnum::ARMOR:
-            this->updateComponent<Armor>(1);
+            this->updateComponent<Armor>(args[0]);
             break;
         case componentsEnum::POSITION:
-            this->updateComponent<Position>(2, 1);
+            this->updateComponent<Position>(args[0], args[1]);
             break;
         case componentsEnum::COLLISION:
-            this->updateComponent<CollisionEffect>();
+            this->updateComponent<CollisionEffect>(args[0], args[1]);
             break;
         default:
             throw ECSError::InvalidComponent(componentName);
