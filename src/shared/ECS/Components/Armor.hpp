b940fd91a92ad1a8e247901f899fc62c3b2b8945
nLatt <56591727+nLatt@users.noreply.github.com>
2022-10-05 12:05:58 +0200

    [Feature] ECS Serialization (#23)
    * [Refactor] Refactor ECS folder structure
    
    * [Refactor] deletes old client folder
    
    * [Style] disable oneline functions
    
    * [Feature] every component serializes and deserializes
    
    * [Feature] add updating off a component via string deserialization
    
    * [Style] update .GITIGNORE
    
    * [Fix] Component is now abstract
    
    * [Fix] removed unneeded includes
    
    * [Fix] added sstream header
    
    * [Doc] document serialization output
    
    Co-authored-by: Léon Sautour <leon@sautour.net>

---
+++ src/shared/ECS/Components/Armor.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2022
+** Armor.hpp
+** File description:
+** .
+*/
+
+#pragma once
+
+#include "../Component.hpp"
+#include "../Entity.hpp"
+
+// Armor component
+// Armor is used to modify behaviour when mutating health component
+class Armor : public Component {
+  private:
+    int _armor = 0;
+
+  public:
+    void init() {
+        this->_armor = 0;
+    }
+
+    void setValues(int armor) {
+
+        this->_armor = armor;
+    }
+
+    int reduceDamage(int damage) {
+        return damage - this->_armor >= 0 ? damage - this->_armor : 0;
+    }
+
+    std::string serialize() const {
+        return "Armor(" + std::to_string(this->_armor) + ")";
+    }
+
+    void update(int armor) {
+        this->_armor = armor;
+    }
+};
