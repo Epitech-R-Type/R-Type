45b5a73b9416b49f93bcc098101959c52b60a536
Nydragon <6582485n@gmail.com>
2022-10-08 15:41:53 +0200

    [Feature] add and test implementation of Cmake RC with Raylib

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,51 +1,70 @@
 #include "../shared/ECS/ECS.hpp"
+#include "raylib.h"
+#include <cmrc/cmrc.hpp>
 #include <iostream>
 
+CMRC_DECLARE(client);
+
+class GameManager {
+  public:
+    GameManager() = default;
+    ~GameManager() = default;
+
+    Texture2D loadSprite(const std::string path, const unsigned int xpos, const unsigned int ypos, const unsigned int xlen, const unsigned int ylen) {
+
+        const cmrc::file image = this->_fs.open(path);
+
+        const unsigned char* imageBuffer = (unsigned char*)(image.begin());
+
+        Image sprite = LoadImageFromMemory("png", imageBuffer, image.size());
+
+        ImageCrop(&sprite, (Rectangle){xpos, ypos, xlen, ylen});
+
+        Texture2D texture = LoadTextureFromImage(sprite);
+
+        UnloadImage(sprite);
+
+        return texture;
+    }
+
+  private:
+    cmrc::embedded_filesystem _fs = cmrc::client::get_filesystem();
+};
+
 int main() {
-    std::cout << "Client: Hello, World!" << std::endl;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    Manager manager;
-    auto& newPlayer(manager.addEntity());
+    InitWindow(screenWidth, screenHeight, "R-Type");
 
-    newPlayer.addComponent<Position>();
+    GameManager* gameManager = new GameManager();
 
-    std::cout << "x: " << newPlayer.getComponent<Position>().x() << ", y: " << newPlayer.getComponent<Position>().y() << std::endl;
+    Texture2D texture = gameManager->loadSprite("resources/r-typesheet1.png", 300, 60, 50, 50);
+    //---------------------------------------------------------------------------------------
 
-    std::cout << newPlayer.isActive() << std::endl;
-    newPlayer.destroy();
-    std::cout << newPlayer.isActive() << std::endl;
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        BeginDrawing();
 
-    auto& zorg(manager.addEntity());
+        ClearBackground(RAYWHITE);
 
-    newPlayer.addComponent<Health>();
+        DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2 - 40, WHITE);
+        DrawRectangleLines(screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2 - 40, texture.width, texture.height, DARKGRAY);
 
-    newPlayer.getComponent<Health>().setValues(10);
+        DrawText("We are drawing only one texture from various images composed!", 240, 350, 10, DARKGRAY);
+        DrawText("Source images have been cropped, scaled, flipped and copied one over the other.", 190, 370, 10, DARKGRAY);
 
-    zorg.addComponent<Health>();
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
 
-    zorg.getComponent<Health>().setValues(10);
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadTexture(texture); // Texture unloading
 
-    zorg.addComponent<CollisionEffect>();
-
-    newPlayer.addComponent<CollisionEffect>();
-
-    newPlayer.getComponent<CollisionEffect>().setEffect(1, 0);
-
-    zorg.getComponent<CollisionEffect>().setEffect(3, 0);
-
-    std::cout << newPlayer.getComponent<Health>().getHealth() << zorg.getComponent<Health>().getHealth() << std::endl;
-
-    zorg.getComponent<CollisionEffect>().collide(newPlayer);
-
-    std::cout << newPlayer.getComponent<Health>().getHealth() << zorg.getComponent<Health>().getHealth() << std::endl;
-
-    newPlayer.deserialize("(Health(2),Armor(2),Position(1,2))");
-
-    std::cout << newPlayer << std::endl;
-
-    newPlayer.deserialize("(Health(40),Armor(4),Position(5,-2))");
-
-    std::cout << newPlayer << std::endl;
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
