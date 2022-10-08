f9e07dd60a0eb65ed3e5394229e8413360c29146
Leon Sautour <leon@sautour.net>
2022-10-08 18:49:37 +0200

    [Refacto] New ECS (#35)
    * [Feature] ECS Serialization (#23)
    
    * [Refactor] Refactor ECS folder structure
    
    * [Refactor] deletes old client folder
    
    * [Style] disable oneline functions
    
    * [Feature] every component serializes and deserializes
    
    * [Feature] add updating off a component via string deserialization
    
    * [Style] update .GITIGNORE
    
    * [Fix] Component is now abstract
    
    * [Fix] removed unneeded includes
    
    * [Fix] added sstream header
    
    * [Doc] document serialization output
    
    Co-authored-by: Léon Sautour <leon@sautour.net>
    
    * [Refactor] add utility typedefs + inline bit manipulation methods to createId, getVersion and getIndex"
    
    * [Feature] add Manager and CompPool class implementation
    
    * [Build] fix build issues for current WIP of ECS refactor
    
    * [Fix] change typedefs to use signed long long and fix deleted entity check
    
    * [Feature] add WIP iterator implementation
    
    * [Fix] fixes iterator build issues
    
    * [Feature] WIP iterator build errors
    
    * [Fix] fix for leon
    
    * [Fix] add to the reasons I shouldn't be a developer
    
    Co-authored-by: nLatt <56591727+nLatt@users.noreply.github.com>
    Co-authored-by: Nydragon <6582485n@gmail.com>

--- src/shared/ECS/ECS.hpp
+++ src/shared/ECS/ECS.hpp
@@ -7,19 +7,54 @@
 
 #pragma once
 
-// This file is a utilitarian header allowing to import :
-//      - All components in Components/
-//      - Entity class
-//      - Manager class
-//      - Component class
+// CONSTANTS
+#define MAX_ENTITIES 1024
+#define MAX_COMPONENTS 4
 
-// Components
-#include "Components/Armor.hpp"
-#include "Components/CollisionEffect.hpp"
-#include "Components/Health.hpp"
-#include "Components/Position.hpp"
+#define INVALID_INDEX -1
 
-// ECS Classes
-#include "Component.hpp"
-#include "Entity.hpp"
-#include "Manager.hpp"
+// TYPE DEFINITIONS
+
+// Type used to represent entity ID
+// Using "long long" as it sits in 8B of memory
+// Allowing us to use 4B for the entity index
+// and 4B for the entity version
+typedef long long Id;
+
+// Readability typedefs
+// Each of these sit in 4B of memory
+typedef long Index;
+typedef long Version;
+
+// GLOBALS
+extern Index g_idCounter; // Used by getID function
+
+// FUNCTIONS
+
+// This function returns a unique ID for each type specified in template
+// parameter
+// NOTE: Do not forget to initialize global in main
+template <typename T>
+Index getID() {
+    static Index outputID = g_idCounter++; 
+    return outputID;
+}
+
+// Extracts Index from ID from top 4B
+inline Index getIndex(Id id) {
+    return (id >> 32);
+}
+
+// Extracts Version from ID from bottom 4B
+inline Version getVersion(Id id) {
+    return (Version)id;
+}
+
+// Create Id from Index and Version
+inline Id createId(Index i, Version v) {
+    Id new_id = i;          // Put index in new_id
+    new_id = new_id << 32;  // Shift index to top bytes
+    new_id = new_id | v;    // Add version to bottom bytes
+
+    return new_id;
+}
