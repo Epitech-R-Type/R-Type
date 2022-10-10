179c9a56fe00da4510e416dd7ca751e9aa06b8ce
Léon Sautour <leon@sautour.net>
2022-10-10 13:09:33 +0200

    [Feature] adds Client class

---
+++ src/client/Client.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** client.cpp
+** File description:
+** Client
+*/
+
+#include "Client.hpp"
+
+Client::Client()
+{
+}
+
+Client::~Client()
+{
+}
---
+++ src/client/Client.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** Client.hpp
+** File description:
+** .
+*/
+
+#pragma once
+
+#include "../shared/ECS/Manager.hpp"
+
+class Client {
+    public:
+        Client();
+        ~Client();
+
+    private:
+};
