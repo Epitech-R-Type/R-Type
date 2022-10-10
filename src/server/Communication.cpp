1234e05e457ff226a9b58f189028de5900838779
Léon Sautour <leon@sautour.net>
2022-10-10 20:08:10 +0200

    [Fix] fix build issues after ConnectionManager removal. Note: All changes remain untested

--- src/server/Communication.cpp
+++ src/server/Communication.cpp
@@ -8,7 +8,7 @@
 #include "Communication.hpp"
 
 // Function passed to communication thread on creation
-void communication_main(std::shared_ptr<MessageQueue<Message>> incoming, std::shared_ptr<MessageQueue<Message>> outgoing) {
+void communication_main(std::shared_ptr<MessageQueue<std::string>> incoming, std::shared_ptr<MessageQueue<std::string>> outgoing) {
     Communication com(incoming, outgoing);
 
     // Setup incoming udp packet handler and outgoing packets handler in asio
@@ -19,9 +19,8 @@ void communication_main(std::shared_ptr<MessageQueue<Message>> incoming, std::sh
     com.run();
 }
 
-Communication::Communication(std::shared_ptr<MessageQueue<Message>> incoming, std::shared_ptr<MessageQueue<Message>> outgoing):
-    _sock(_ctxt, asio::ip::udp::endpoint(asio::ip::udp::v6(), 3500)), _t(_ctxt, asio::chrono::milliseconds(10))
-{
+Communication::Communication(std::shared_ptr<MessageQueue<std::string>> incoming, std::shared_ptr<MessageQueue<std::string>> outgoing)
+    : _sock(_ctxt, asio::ip::udp::endpoint(asio::ip::udp::v6(), 3500)), _t(_ctxt, asio::chrono::milliseconds(10)) {
     this->_incomingMessages = incoming;
     this->_outgoingMessages = outgoing;
 }
@@ -38,9 +37,7 @@ void Communication::setup_incoming_handler()
             auto addr = this->_endpoint.address();
             auto port = this->_endpoint.port();
 
-            int conn_id = this->getConnectionManager().getClientId(addr, port);
-
-            this->push_message({conn_id, this->_buffer});
+            this->push_message(Message(std::string(this->_buffer), addr, port));
 
             //Reset buffer
             memset(this->_buffer, 0, 1024);
@@ -67,37 +64,30 @@ void Communication::setup_outgoing_handler()
             this->setup_outgoing_handler();
             return;
         }
-        std::optional<Message> msg;
+        std::optional<Message<std::string>> msg;
         char buffer[1024];
 
         if ((msg = this->pop_message())) {
-            auto addr = this->getConnectionManager().getClientAddr(msg->client_id);
 
-            if (!addr) {
-                std::cerr << "Cannot send message, client not in ConnectionsManager" << std::endl;
-                this->setup_outgoing_handler();
-                return;
-            }
+            std::string msgStr = msg->getMsg();
 
             // Prepare buffer
-            int len = msg->msg.length();
-            strcpy(buffer, msg->msg.c_str());
+            int len = msgStr.length();
+            strcpy(buffer, msgStr.c_str());
             memset(&buffer[len], 0, 1024 - len);
 
-            asio::ip::udp::endpoint target(addr->ip, addr->port);
+            asio::ip::udp::endpoint target(msg->getAddr(), msg->getPort());
             this->_sock.send_to(asio::buffer(buffer), target);
         }
         this->setup_outgoing_handler();
     });
 }
 
-void Communication::push_message(Message msg)
-{
+void Communication::push_message(Message<std::string> msg) {
     this->_incomingMessages->push(msg);
 }
 
-std::optional<Message> Communication::pop_message(void)
-{
+std::optional<Message<std::string>> Communication::pop_message(void) {
     return this->_outgoingMessages->pop();
 }
 
