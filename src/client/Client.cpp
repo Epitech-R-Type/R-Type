a0c6ed0707665d17545a356d90eb0e12ce140d2e
Léon Sautour <leon@sautour.net>
2022-10-13 10:11:31 +0200

    [Build] fix build issues

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -9,8 +9,10 @@
 
 int Client::launchGame() {
     // Note: For performance reasons we could free the lobby ecs before launching the game
-    this->_game = ClientGame();
+    // this->_game = ClientGame();
     this->_game.mainLoop();
+
+    return 0;
 }
 
 int Client::mainLoop() {
@@ -19,6 +21,8 @@ int Client::mainLoop() {
         // Use input from user to navigate menu
         // Manipulate local ecs
     }
+
+    return 0;
 }
 
 int Client::setup() {
