6b3a279a8571dfc1e828caeee1afcc971fd94bbf
Nydragon <6582485n@gmail.com>
2022-10-13 10:52:26 +0200

    [Fix] remove include

--- src/client/Client.hpp
+++ src/client/Client.hpp
@@ -7,7 +7,6 @@
 
 #pragma once
 
-#include "../shared/ECS/Manager.hpp"
 #include "ClientGame/ClientGame.hpp"
 
 // This class implementls the main loop for lobby handling of the client
@@ -15,21 +14,21 @@
 // As soon as a game start, this class will instantiate the ClientGame class
 // responsible of everything pertaining to a given day
 class Client {
-    public:
-        // Note: Construtor/Destructor shall be added as needed
+public:
+    // Note: Construtor/Destructor shall be added as needed
 
-        // Intialization of game lobby
-        int setup();
+    // Intialization of game lobby
+    int setup();
 
-        // Menu main loop
-        // Return 0 if success or 1 if failure
-        int mainLoop();
+    // Menu main loop
+    // Return 0 if success or 1 if failure
+    int mainLoop();
 
-        // Launches new game
-        // Return is 0 if success or 1 if failure
-        int launchGame();
+    // Launches new game
+    // Return is 0 if success or 1 if failure
+    int launchGame();
 
-    private:
-        bool _lobbyRunning;
-        ClientGame _game;
+private:
+    bool _lobbyRunning;
+    ClientGame _game;
 };
