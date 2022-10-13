88c1eef72772147f4b4eed5b3cad4505a28e70c0
Nydragon <6582485n@gmail.com>
2022-10-13 10:45:23 +0200

    [Fix] remove headers

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,11 +1,4 @@
-#include "../shared/ECS/ECS.hpp"
-#include "../shared/ECS/Manager.hpp"
-#include "../shared/systems/UUID.hpp"
-#include "./systems/Systems.hpp"
 #include "Client.hpp"
-#include "raylib.h"
-
-#include <iostream>
 
 Index g_idCounter = 0;
 
