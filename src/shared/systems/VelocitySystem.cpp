39b5edd19cccc4c91f9d5dc935cc50958566135a
nLatt <56591727+nLatt@users.noreply.github.com>
2022-10-13 21:52:46 +0200

    46 add client input (#56)
    * [Feature] added new system for player movement
    
    * [Fix] added check for screen end to player movement
    
    * [Feature] add collision for map, add render layers
    
    * [Fix] move raylib initi to main
    
    * [Fix] readd excluded in view
    
    * [Fix] remove headers
    
    * [Fix] add includes
    
    * [Feature] add windows header
    
    * [Fix] windows build
    
    * [Fix] fix
    
    * [Fix] when you fix something like a big boy
    
    Co-authored-by: Sebastian Klemm <sebastian.klemm@epitech.eu>

--- src/shared/systems/VelocitySystem.cpp
+++ src/shared/systems/VelocitySystem.cpp
@@ -10,12 +10,11 @@ VelocitySystem::VelocitySystem(Manager* ECS) {
 void VelocitySystem::apply() {
     for (auto beg = this->_ECS->begin<Position::Component, Velocity::Component>(); beg != this->_ECS->end<Position::Component, Velocity::Component>();
          ++beg) {
+
         EntityID id = *beg;
+
         Velocity::Component* velocity = this->_ECS->getComponent<Velocity::Component>(id);
         Position::Component* position = this->_ECS->getComponent<Position::Component>(id);
-
-        if (!velocity || !position)
-            continue;
 
         position->xPos += velocity->xVelocity;
         position->yPos += velocity->yVelocity;
