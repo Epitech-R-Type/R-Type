4150bb8a5c12207cc129eac43983e8e116c7bf20
Nydragon <6582485n@gmail.com>
2022-10-14 10:43:14 +0200

    [Doc] add comments to UUID

--- src/shared/systems/UUID.hpp
+++ src/shared/systems/UUID.hpp
@@ -7,10 +7,19 @@
 
 class UUID {
 public:
+    /**
+     * Generate a new UUID
+     * */
     UUID();
 
+    /**
+     * Extract the first UUID of a string
+     * */
     UUID(std::string uuidStr);
 
+    /**
+     * Cast UUID to string, to send it over network or similar
+     * */
     std::string toString();
 
     bool operator==(const UUID& uuid);
