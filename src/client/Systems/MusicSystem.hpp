f0456f2440a8960cdaae122af645b55387d242bc
klemmse <71702113+klemmse@users.noreply.github.com>
2022-10-31 12:44:25 +0100

    92 add music (#99)
    * [Feature] added musicSystem; increased raylib version to 4.0.0; fixed bug that came with version change
    
    * [Assets] added new chill song
    
    * [Fix] fixed compile resources
    
    * [Fix] removed comment

---
+++ src/client/Systems/MusicSystem.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** MusicSystem
+*/
+
+#pragma once
+
+#include <raylib.h>
+#include <cmrc/cmrc.hpp>
+#include "../../shared/Systems/ISystem.hpp"
+
+CMRC_DECLARE(client);
+
+struct Song {
+    const std::string path;
+};
+
+static std::vector<Song> Songs {
+    {"resources/song0.mp3"},
+    {"resources/song1.mp3"}
+};
+
+class MusicSystem : public System {
+    public:
+        MusicSystem(int ID);
+        ~MusicSystem();
+        void apply();
+        void changeSong(int ID);
+
+    private:
+        Music _music;
+        float _volume = 0.5;
+        cmrc::embedded_filesystem _fs = cmrc::client::get_filesystem();
+};
+
