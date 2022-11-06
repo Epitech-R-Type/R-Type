dae67696fb16dbe96bca83cdfea6c8ed9f821651
Léon Sautour <leon@sautour.net>
2022-11-06 15:18:52 +0100

    [Style] split up TCP command handling server side into different functions

--- src/server/Protocols/LobbyProtocol.hpp
+++ src/server/Protocols/LobbyProtocol.hpp
@@ -34,16 +34,25 @@ public:
     // Sends start game command to every connected client
     void startGame(int port);
 
-    // Utility function to check if users UUID is valid
-    bool isAuthenticated(std::string uuid);
+    // ─── Handle Commands And Responde ────────────────────────────────────────────────────────
 
     // Handle commands
     // Returns true if game should start
     int handleCommands();
 
+    void handleWrongRequest(std::string msgBody, asio::ip::address addr, asio::ip::port_type port);
+    void handleForbidden(asio::ip::address addr, asio::ip::port_type port);
+    void handleConnect(asio::ip::address addr, asio::ip::port_type port);
+    int handleStart();
+
+    // ─── Utility Functions ───────────────────────────────────────────────────────────────────
+
     // Utility function in order to form response automatically
     // Handles putting uuid in and str forming
     void sendResponse(std::string code, std::string args, asio::ip::address addr, asio::ip::port_type port);
+
+    // Utility function to check if users UUID is valid
+    bool isAuthenticated(std::string uuid);
 
     std::vector<Connection> getConnections();
 
