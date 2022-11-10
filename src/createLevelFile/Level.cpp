366fe4569e4c4bf877049162ba185ee10e9158cc
Sebastian Klemm <sebastian.klemm@epitech.eu>
2022-11-10 14:15:51 +0100

    [Feature] create level file function updated; has weird error that i don't know how to fix

--- src/createLevelFile/Level.cpp
+++ src/createLevelFile/Level.cpp
@@ -14,137 +14,236 @@
 
 #include "Level.hpp"
 
-bool Level::nextLvl()
-{
-    this->clear();
-    this->currentLvl += 1;
-    std::string name("./levels/level"), line;
-    name.append(std::to_string(currentLvl));
-    std::fstream lvlFile(name, std::ios_base::in);
-    Wave tmp;
-    if (lvlFile.is_open() == false)
-        return false;
-    std::getline(lvlFile, line);
-    this->mapId = std::atoi(line.c_str());
-    std::getline(lvlFile, line);
-    this->mapSpeed = std::atoi(line.c_str());
-    std::getline(lvlFile, line);
-    this->bossCountdown = std::atoi(line.c_str());
-
-    while (std::getline(lvlFile, line)) {
-        tmp.minSpawned = std::atoi(line.c_str());
-        std::getline(lvlFile, line);
-        tmp.maxSpawned = std::atoi(line.c_str());
-        std::getline(lvlFile, line);
-        tmp.typesFlag = std::atoi(line.c_str());
-        std::getline(lvlFile, line);
-        tmp.spawnInterval = std::atof(line.c_str());
-        this->levelWaves.push_back(tmp);
-    }
-    lvlFile.close();
-    return true;
+bool isFloat(std::string myString) {
+    std::istringstream iss(myString);
+    float f;
+    iss >> std::noskipws >> f;
+    return iss.eof() && !iss.fail(); 
 }
 
-void Level::loadLvlNb(int nb)
+std::string Level::getBackground()
 {
-    std::fstream lvlFile(std::string("./levels/level").append(std::to_string(nb)), std::ios_base::in);
-    std::string line;
-    Wave tmp;
-    tmp.finishedFlag = false;
-    tmp.endless = false;
-    this->currentLvl = 1;
+    int finished = 0;
+    std::string bg;
 
-    std::getline(lvlFile, line);
-    this->mapId = std::atoi(line.c_str());
-    std::getline(lvlFile, line);
-    this->mapSpeed = std::atoi(line.c_str());
-    std::getline(lvlFile, line);
-    this->bossCountdown = std::atoi(line.c_str());
-
-    while (std::getline(lvlFile, line)) {
-        tmp.minSpawned = std::atoi(line.c_str());
-        std::getline(lvlFile, line);
-        tmp.maxSpawned = std::atoi(line.c_str());
-        std::getline(lvlFile, line);
-        tmp.typesFlag = std::atoi(line.c_str());
-        std::getline(lvlFile, line);
-        tmp.spawnInterval = std::atof(line.c_str());
-        srand(time(NULL));
-        tmp.spawned = rand() % tmp.maxSpawned + tmp.minSpawned;
-        this->levelWaves.push_back(tmp);
+    while (!finished) {
+        std::cout << "What background should we use?" << std::endl;
+        bg = "";
+        std::cin >> bg;
+        for (int i = 0; i != bg.size() - 1; i++)
+            if (!isdigit(bg[i]))
+                continue;
+        if (std::find(std::begin(this->_backGrounds), std::end(this->_backGrounds), std::stoi(bg)) != std::end(this->_backGrounds))
+            finished = 1;
     }
-    lvlFile.close();
-    this->currentLvl = 1;
+    return (bg);
+}
+
+std::string Level::getBossSpawnTimer()
+{
+    int finished = 0;
+    std::string timer;
+
+    while (!finished) {
+        std::cout << "After how many seconds should the boss show up?" << std::endl;
+        timer = "";
+        std::cin >> timer;
+        finished = 1;
+        for (int i = 0; i != timer.size() - 1; i++)
+            if (!isdigit(timer[i])) {
+                finished = 0;
+                continue;
+            }
+        if (std::stoi(timer) < 0)
+            finished = 0;
+    }
+    return (timer);
+}
+
+std::string Level::getMusic()
+{
+    int finished = 0;
+    std::string music;
+
+    while (!finished) {
+        std::cout << "What Music should be played when the boss hasn't spawned yet?" << std::endl;
+        music = "";
+        std::cin >> music;
+        for (int i = 0; i != music.size() - 1; i++)
+            if (!isdigit(music[i]))
+                continue;
+        if (std::stoi(music) >= 0 || std::stoi(music) < this->_songs)
+            finished = 1;
+    }
+    return (music);
+}
+
+std::string Level::getMin()
+{
+    int finished = 0;
+    std::string min;
+
+    while (!finished) {
+        std::cout << "How many enemies should there be at least?" << std::endl;
+        min = "";
+        std::cin >> min;
+        finished = 1;
+        for (int i = 0; i != min.size() - 1; i++)
+            if (!isdigit(min[i])) {
+                finished = 0;
+                continue;
+            }
+        if (std::stoi(min) < 1)
+            finished = 0;
+    }
+    return (min);
+}
+
+std::string Level::getMax(int min)
+{
+    int finished = 0;
+    std::string max;
+
+    while (!finished) {
+        std::cout << "How many enemies should there be at max?" << std::endl;
+        max = "";
+        std::cin >> max;
+        finished = 1;
+        for (int i = 0; i != max.size() - 1; i++)
+            if (!isdigit(max[i])) {
+                finished = 0;
+                continue;
+            }
+        if (std::stoi(max) < min)
+            finished = 0;
+    }
+    return (max);
+}
+
+std::string Level::getInterval()
+{
+    int finished = 0;
+    std::string interval;
+
+    while (!finished) {
+        std::cout << "What should the spawn interval be?" << std::endl;
+        interval = "";
+        std::cin >> interval;
+        if (!isFloat(interval))
+            continue;
+        if (std::stof(interval) > 0)
+            finished = 1;
+    }
+    return (interval);
+}
+
+std::string Level::getEnemy()
+{
+    int finished = 0;
+    int finished2 = 0;
+    std::string enemy;
+    std::vector<int> enemys;
+
+    while (!finished) {
+        while (!finished2) {
+            enemy = "";
+            std::cout << "What Enemy should spawn?" << std::endl;
+            std::cin >> enemy;
+            for (int i = 0; i != enemy.size() - 1; i++)
+                if (!isdigit(enemy[i]))
+                    continue;
+            if (std::stoi(enemy) >= 0 && std::stoi(enemy) <= this->_enemys && std::find(std::begin(enemys), std::end(enemys), std::stoi(enemy)) == std::end(enemys)) {
+                finished2 = 1;
+                enemys.push_back(std::stoi(enemy));
+            }
+        }
+        finished2 = 0;
+        std::cout << "More Enemys? (yes | no is everything else)" << std::endl;
+        enemy = "";
+        std::cin >> enemy;
+        if (enemy != "yes" || enemys.size() > this->_enemys)
+            finished = 1;
+    }
+    enemy = "";
+    for (int i = 0; i <= enemys.size(); i++)
+        enemy += enemys[i] + " ";
+    enemy.pop_back();
+    return(enemy);
+}
+
+std::string Level::getBoss()
+{
+    int finished = 0;
+    std::string boss;
+
+    while (!finished) {
+        std::cout << "What Boss should be here?" << std::endl;
+        boss = "";
+        std::cin >> boss;
+        if (boss == "-1")
+            break;
+        for (int i = 0; i != boss.size() - 1; i++)
+            if (!isdigit(boss[i]))
+                continue;
+        if (std::stoi(boss) < 0 || std::stoi(boss) > this->_boss)
+            finished = 1;
+    }
+    return (boss);
+}
+
+int Level::getWaves()
+{
+    int finished = 0;
+    std::string waves;
+
+    while (!finished) {
+        std::cout << "How many enemy waves should there be?" << std::endl;
+        waves = "";
+        std::cin >> waves;
+        for (int i = 0; i != waves.size() - 1; i++)
+            if (!isdigit(waves[i]))
+                continue;
+        if (std::stoi(waves) > 0)
+            finished = 1;
+    }
+    return (std::stoi(waves));
 }
 
 void Level::createLvl()
 {
-    int nb = 0;
     int count;
-    Wave tmp;
-    Level toWrite;
+    std::string toWrite;
     std::string str;
-    tmp.finishedFlag = false;
-    tmp.endless = false;
 
-    std::cout << "What background should we use?" << std::endl;
-    std::cin >> toWrite.mapId;
-    std::cout << "How fast should the background be?" << std::endl;
-    std::cin >> toWrite.mapSpeed;
-    std::cout << "After how many seconds should the boss show up?" << std::endl;
-    std::cin >> toWrite.bossCountdown;
-    std::cout << "How many enemy waves should there be?" << std::endl;
-    std::cin >> count;
+    toWrite += this->getBackground() + '\n';
+    toWrite += this->getMusic() + '\n';
+    toWrite += '\n';
+    count = this->getWaves();
 
     for (int i = 0; i < count; i++) {
-        tmp.typesFlag = 0;
-        tmp.maxSpawned = 0;
-        tmp.minSpawned = 0;
-        tmp.spawnInterval = 0.0;
         std::cout << "\n   Describe wave nb " << i+1 << std::endl;
-        std::cout << "How many enemies should there be at least?" << std::endl;
-        std::cin >> tmp.minSpawned;
-        std::cout << "How many enemies should there be at max?" << std::endl;
-        std::cin >> tmp.maxSpawned;
-        std::cout << "Should there be enemies of type ORB?";
-
-        for (int x = 0; tmp.typesFlag == 0; x++) {
-            std::cout << " [+/-]" << std::endl;
-            std::cin >> str;
-            if (str.compare("+") == 0) {
-                tmp.typesFlag += 1;
-            } else if (str.compare("-") == 0);
-                std::cout << "--We need more enemy types!--" << std::endl;
-        }
-        std::cout << "what should the spawn interval be?" << std::endl;
-        std::cin >> tmp.spawnInterval;
-        toWrite.levelWaves.push_back(tmp);
+        str = this->getMin();
+        toWrite += str + '\n';
+        toWrite += this->getMax(std::stoi(str)) + '\n';
+        toWrite += this->getInterval() + '\n';
+        toWrite += this->getEnemy() + '\n';
+        toWrite += this->getBoss() + '\n';
+        toWrite += this->getBossSpawnTimer() + '\n';
+        toWrite += '\n';
     }
-    std::fstream LvlFile = this->createFile();
-    this->writeLvlToFile(LvlFile, toWrite);
+    toWrite.pop_back();
+    toWrite.pop_back();
+    std::fstream LevelFile = this->createFile();
+    LevelFile << toWrite;
+    LevelFile.close();
 }
 
 std::fstream Level::createFile()
 {
     std::string name("./src/server/levels/level");
     name.append(std::to_string(this->countLvls()));
-    std::fstream LvlFile(name, std::fstream::out);
-    return (LvlFile);
-}
-
-void Level::writeLvlToFile(std::fstream &lvlFile, Level &toWrite)
-{
-    lvlFile << toWrite.mapId << std::endl;
-    lvlFile << toWrite.mapSpeed << std::endl;
-    lvlFile << toWrite.bossCountdown << std::endl;
-
-    for (auto i : toWrite.levelWaves) {
-        lvlFile << i.minSpawned << std::endl;
-        lvlFile << i.maxSpawned << std::endl;
-        lvlFile << i.typesFlag << std::endl;
-        lvlFile << i.spawnInterval << std::endl;
-    }
-    lvlFile.close();
+    std::fstream LevelFile(name, std::fstream::out);
+    return (LevelFile);
 }
 
 int Level::countLvls()
@@ -156,62 +255,4 @@ int Level::countLvls()
         if (entry.is_regular_file())
             ++fileCount;
     return fileCount+1;
-}
-
-void Level::print()
-{
-    std::cout << "mapId=" << this->mapId << std::endl;
-    std::cout << "mapSpeed=" << this->mapSpeed << std::endl;
-    std::cout << "bossCountdown=" << this->bossCountdown << std::endl;
-    std::cout << std::endl;
-
-
-    for (int i = 0; i < this->levelWaves.size(); i++) {
-    std::cout << "-- content for wave " << i+1 << " --"<< std::endl;
-        std::cout << "minSpawned=" << levelWaves[i].minSpawned << std::endl;
-        std::cout << "maxSpawned=" << levelWaves[i].maxSpawned << std::endl;
-        std::cout << "typesFlag=" << levelWaves[i].typesFlag << std::endl;
-        std::cout << "spawnInterval=" << levelWaves[i].spawnInterval << std::endl << std::endl;
-    }
-}
-
-void Level::clear()
-{
-    this->bossCountdown = 0;
-    this->levelWaves.clear();
-    this->mapId = 0;
-    this->mapSpeed = 0;
-}
-
-void Level::startLvl()
-{
-    this->clock.start();
-    this->waveNb = 0;
-}
-
-void Level::exec()
-{
-    int spawns = 0;
-    double intervals = (double)clock.interval().count();
-    int types = levelWaves[waveNb].typesFlag;
-    spawns = intervals;
-
-    if (intervals > levelWaves[waveNb].spawnInterval && levelWaves[waveNb].spawned > 0) {
-        clock._enemy = GetNow();
-        std::cout << "spawning enemy\n"; // create enemy here
-        levelWaves[waveNb].spawned--;
-    }
-    if (clock.boss().count() > bossCountdown) {
-        clock._boss = GetNow();
-        std::cout << "spawning boss\n"; // create boss here
-    }
-    // if (allEnemiesDead) { //pseudo code
-    //     waveNb += 1;
-    //     if (waveNb >= levelWaves.size() && bossDead)
-    //         if (this->nextLvl() == false)
-    //             over = true;
-    //         else
-    //             currentLvl += 1;
-    // }
-    return;
 }
--- src/createLevelFile/Level.hpp
+++ src/createLevelFile/Level.hpp
@@ -15,59 +15,30 @@
 #include <random>
 #include <fstream>
 #include <filesystem>
-
-typedef std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds> timePoint;
-
-inline auto GetNow() {
-    return std::chrono::time_point_cast<timePoint::duration>(std::chrono::system_clock::time_point(std::chrono::system_clock::now()));
-};
-
-// class decribing an enemy wave of a level
-class Wave {
-    public:
-        bool finishedFlag;
-        bool endless;
-        int minSpawned;
-        int maxSpawned;
-        float spawnInterval;
-        int spawned;
-        int typesFlag; //use powers of 2 to describe what enemys are contained in the wave to make binary checks later (type1 = 1, type2 = 2, type3 = 4, ...)
-    private:
-};
-
-class LevelClock {
-    public:
-        std::chrono::duration<double> boss() {const auto _now = GetNow(); return (_now - _boss);};
-        std::chrono::duration<double> interval() {const auto _now = GetNow(); return (_now - _enemy);};
-        void start() {this->_enemy = GetNow(); _boss = GetNow();};
-        timePoint _enemy;
-        timePoint _boss;
-    private:
-};
+#include <algorithm>
 
 class Level { // put into vector to use all levels
     public:
         Level() = default;
         ~Level() = default;
-        void loadLvlNb(int nb);
-        bool nextLvl();
-        void startLvl();
-        void exec();
         void createLvl();
-        void print();
-        LevelClock clock;
 
     private:
-        void writeLvlToFile(std::fstream &lvlFile, Level &toWrite);
+        std::vector<int> _backGrounds = {11};
+        int _enemys = 3;
+        int _songs = 2;
+        int _boss = 0;
         std::fstream createFile();
         int countLvls();
-        void clear();
-        std::vector<Wave> levelWaves;
-        int waveNb;
-        int bossCountdown;
-        int mapSpeed;
-        int mapId;
-        int currentLvl;
+        std::string getBackground();
+        std::string getBossSpawnTimer();
+        std::string getMusic();
+        std::string getMin();
+        std::string getMax(int min);
+        std::string getInterval();
+        std::string getEnemy();
+        std::string getBoss();
+        int getWaves();
 };
 
 #endif /* !LEVEL_HPP_ */
