878344863f0ac6d5ebb47948a20e115792692257
Leon Sautour <leon@sautour.net>
2022-11-11 18:47:01 +0100

    83 implement multitple lobby handling (#134)
    * [Feature] implement gamesToLaunch in preparation of multi lobby handling
    
    * [Feature] add execution of game in separate thread
    
    * [Feature] implement JOIN_LOBBY command
    
    * [Build] update client config for debugger
    
    * [Feature] add lobby number parsing in client
    
    * [Fix] fix game exiting immediately after start game command
    
    * [Fix] fixed isPortAvailable() not checking for udp port availability

--- src/server/Protocols/LobbyProtocol.hpp
+++ src/server/Protocols/LobbyProtocol.hpp
@@ -24,25 +24,30 @@
 // Server Commands
 #define START_GAME "START"
 
+struct GameInfo {
+    int lobbyId;
+    int port;
+};
+
 class LobbyProtocol {
 public:
     LobbyProtocol(std::shared_ptr<MessageQueue<Message<std::string>>> incoming, std::shared_ptr<MessageQueue<Message<std::string>>> outgoing,
-                  Utilities::UUID serverUUID);
+                  Utilities::UUID serverUUID, std::shared_ptr<std::vector<GameInfo>> gamesToLaunch);
 
     // Server Commands
     // Sends start game command to every connected client
-    void startGame(int port);
+    void startGame(int port, int lobby);
 
     // ─── Handle Commands And Responde ────────────────────────────────────────────────────────
 
     // Handle commands
-    // Returns true if game should start
-    int handleCommands();
+    void handleCommands();
 
     void handleWrongRequest(std::string msgBody, asio::ip::address addr, asio::ip::port_type port);
     void handleForbidden(asio::ip::address addr, asio::ip::port_type port);
     void handleConnect(asio::ip::address addr, asio::ip::port_type port);
-    int handleStart();
+    void handleJoinLobby(asio::ip::address addr, asio::ip::port_type port, Utilities::UUID clientUUID, int lobby);
+    void handleStart(Utilities::UUID uuid, asio::ip::address addr, asio::ip::port_type port);
 
     // ─── Utility Functions ───────────────────────────────────────────────────────────────────
 
@@ -53,7 +58,12 @@ public:
     // Utility function to check if users UUID is valid
     bool isAuthenticated(std::string uuid);
 
+    // Retrieve all connected clients
     std::vector<Connection> getConnections();
+
+    // ─── Getters And Setters ─────────────────────────────────────────────────────────────────
+
+    ConnectionManager& getConnectionManager();
 
 private:
     // Connection manager
@@ -62,4 +72,7 @@ private:
     // Messaging Queues
     std::shared_ptr<MessageQueue<Message<std::string>>> _incomingMQ;
     std::shared_ptr<MessageQueue<Message<std::string>>> _outgoingMQ;
+
+    // Games to launch reference
+    std::shared_ptr<std::vector<GameInfo>> _gamesToLaunch;
 };
